045dd42f57a4e5a7cbd08ae5eeee0c8e565a3dba
ljw0829 <olo_jinwon@naver.com>
2022-07-21 22:37:58 +0900

    - feat : 코틀린에서 컬렉션 만들기

---
+++ src/main/kotlin/com/example/kotlin/kotlin_in_action_3/KotlinMethod.kt
@@ -0,0 +1,11 @@
+package com.example.kotlin.kotlin_in_action_3
+
+fun main() {
+
+}
+
+fun collection() {
+    hashSetOf(1, 7, 53)
+    arrayListOf(1, 7, 53)
+    hashMapOf(1 to "ONE", 7 to "SEVEN", 52 to "FIFTY-THREE")
+}
