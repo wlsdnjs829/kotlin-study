0dde2119a89eaecfbd91f87ee8d87c35a65e8905
ljw0829 <olo_jinwon@naver.com>
2022-07-23 21:45:59 +0900

    - feat : 문자열과 정규식 다루기

--- src/main/kotlin/com/example/kotlin/kotlin_in_action_3/KotlinMethod.kt
+++ src/main/kotlin/com/example/kotlin/kotlin_in_action_3/KotlinMethod.kt
@@ -1,7 +1,7 @@
 package com.example.kotlin.kotlin_in_action_3
 
 fun main() {
-
+    kotlinLogo()
 }
 
 fun collection() {
@@ -15,3 +15,38 @@ fun listToString() {
     println(listOf)
 }
 
+fun regexSplit() {
+    println("12.345-6.A".split("[.\\-]".toRegex()))
+}
+
+fun regexSplit2() {
+    println("12.345-6.A".split(".", "-"))
+}
+
+fun parsePath(path: String) {
+    val directory = path.substringBeforeLast("/")
+    val fullName = path.substringAfterLast("/")
+
+    val fileName = fullName.substringBeforeLast(".")
+    val extension = fullName.substringAfterLast(".")
+
+    println("Dir: $directory, Name: $fileName, Ext: $extension")
+}
+
+fun parsePathLib(path: String) {
+    val regex = """(.+)/(.+)\.(.+)""".toRegex()
+    val matchResult = regex.matchEntire(path)
+
+    if (matchResult != null) {
+        val (directory, fileName, extension) = matchResult.destructured
+        println("Dir: $directory, Name: $fileName, Ext: $extension")
+    }
+}
+
+fun kotlinLogo() {
+    val logo = """|  //
+                 .| //
+                 .|// \ """.trimMargin(".")
+
+    println(logo)
+}
