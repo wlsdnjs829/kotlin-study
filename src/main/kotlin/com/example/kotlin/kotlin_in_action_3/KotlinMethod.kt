df3b903a4b5b2bff4f4bb0b5374caa9768a9ad85
ljw0829 <olo_jinwon@naver.com>
2022-07-23 22:15:43 +0900

    - feat : 코드 다듬기: 로컬 함수와 확장

--- src/main/kotlin/com/example/kotlin/kotlin_in_action_3/KotlinMethod.kt
+++ src/main/kotlin/com/example/kotlin/kotlin_in_action_3/KotlinMethod.kt
@@ -1,52 +1,50 @@
 package com.example.kotlin.kotlin_in_action_3
 
-fun main() {
-    kotlinLogo()
-}
+class KotlinMethod {
+    fun collection() {
+        hashSetOf(1, 7, 53)
+        arrayListOf(1, 7, 53)
+        hashMapOf(1 to "ONE", 7 to "SEVEN", 52 to "FIFTY-THREE")
+    }
 
-fun collection() {
-    hashSetOf(1, 7, 53)
-    arrayListOf(1, 7, 53)
-    hashMapOf(1 to "ONE", 7 to "SEVEN", 52 to "FIFTY-THREE")
-}
+    fun listToString() {
+        val listOf = listOf(1, 2, 3)
+        println(listOf)
+    }
 
-fun listToString() {
-    val listOf = listOf(1, 2, 3)
-    println(listOf)
-}
+    fun regexSplit() {
+        println("12.345-6.A".split("[.\\-]".toRegex()))
+    }
 
-fun regexSplit() {
-    println("12.345-6.A".split("[.\\-]".toRegex()))
-}
+    fun regexSplit2() {
+        println("12.345-6.A".split(".", "-"))
+    }
 
-fun regexSplit2() {
-    println("12.345-6.A".split(".", "-"))
-}
+    fun parsePath(path: String) {
+        val directory = path.substringBeforeLast("/")
+        val fullName = path.substringAfterLast("/")
 
-fun parsePath(path: String) {
-    val directory = path.substringBeforeLast("/")
-    val fullName = path.substringAfterLast("/")
+        val fileName = fullName.substringBeforeLast(".")
+        val extension = fullName.substringAfterLast(".")
 
-    val fileName = fullName.substringBeforeLast(".")
-    val extension = fullName.substringAfterLast(".")
-
-    println("Dir: $directory, Name: $fileName, Ext: $extension")
-}
-
-fun parsePathLib(path: String) {
-    val regex = """(.+)/(.+)\.(.+)""".toRegex()
-    val matchResult = regex.matchEntire(path)
-
-    if (matchResult != null) {
-        val (directory, fileName, extension) = matchResult.destructured
         println("Dir: $directory, Name: $fileName, Ext: $extension")
     }
-}
 
-fun kotlinLogo() {
-    val logo = """|  //
+    fun parsePathLib(path: String) {
+        val regex = """(.+)/(.+)\.(.+)""".toRegex()
+        val matchResult = regex.matchEntire(path)
+
+        if (matchResult != null) {
+            val (directory, fileName, extension) = matchResult.destructured
+            println("Dir: $directory, Name: $fileName, Ext: $extension")
+        }
+    }
+
+    fun kotlinLogo() {
+        val logo = """|  //
                  .| //
                  .|// \ """.trimMargin(".")
 
-    println(logo)
+        println(logo)
+    }
 }
---
+++ src/main/kotlin/com/example/kotlin/kotlin_in_action_3/KotlinRefactor.kt
@@ -0,0 +1,27 @@
+package com.example.kotlin.kotlin_in_action_3
+
+fun main() {
+    val user = User(1, "이름", "주소")
+}
+
+class KotlinRefactor {
+
+    private fun User.validateBeforeSave() {
+        fun validate(value: String, fieldName: String) {
+            if (value.isEmpty()) {
+                throw IllegalArgumentException("사용자를 저장 할 수 없다. ${id}: 빈 $fieldName")
+            }
+        }
+
+        validate(name, "이름")
+        validate(address, "주소")
+    }
+
+    fun saveUser(user: User) {
+        user.validateBeforeSave();
+    }
+
+}
+
+class User(val id: Int, val name: String, val address: String)
+
