77077d4984600a6c5e16ecb143b12b9443a39500
ljw0829 <olo_jinwon@naver.com>
2022-07-26 20:29:22 +0900

    - feat : 람다 식과 멤버 참조

---
+++ src/main/kotlin/com/example/kotlin/kotlin_in_action_5/Lambda.kt
@@ -0,0 +1,22 @@
+package com.example.kotlin.kotlin_in_action_5
+
+fun main() {
+
+}
+
+/*
+
+// 무명 내부 클래스
+button.setOnClickLister(new OnClickListener() {
+
+    @Override
+    public void onClick(View view) {
+        /* 클릭 시 수행할 동작 */
+    }
+
+}
+
+// 람다
+button.setOnClickLister { /* 클릭 시 수행할 동작 */ }
+
+*/
