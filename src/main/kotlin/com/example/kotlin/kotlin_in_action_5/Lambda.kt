9a3dce04cc5e0f29529f6b1d8cfdcc413d49f79a
ljw0829 <olo_jinwon@naver.com>
2022-07-26 20:55:05 +0900

    - feat : 람다 식의 문법

--- src/main/kotlin/com/example/kotlin/kotlin_in_action_5/Lambda.kt
+++ src/main/kotlin/com/example/kotlin/kotlin_in_action_5/Lambda.kt
@@ -1,12 +1,22 @@
 package com.example.kotlin.kotlin_in_action_5
 
 fun main() {
-    max()
+    lambdaJoinToString()
 }
 
 fun max() {
     val people = listOf(Person("진원", 28), Person("젊은 진원", 18))
     println(people.maxByOrNull(Person::age))
+}
+
+fun lambdaJoinToString() {
+    val people = listOf(Person("진원", 28), Person("젊은 진원", 18))
+    println(people.joinToString(" ") { it.name } )
+}
+
+fun lambdaVal() {
+    val sum = { x: Int, y: Int -> x + y }
+    println(sum(1, 2))
 }
 
 data class Person(val name: String, val age: Int)
