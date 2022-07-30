3d5e8a680e555c2887b1d28f9dac9084af6c3234
ljw0829 <olo_jinwon@naver.com>
2022-07-30 20:30:02 +0900

    - feat : 나중에 초기화할 프로퍼티

---
+++ src/main/kotlin/com/example/kotlin/kotlin_in_action_6/lateInitialized.kt
@@ -0,0 +1,22 @@
+package com.example.kotlin.kotlin_in_action_6
+
+class MyService {
+    fun performAction(): String = "JinWon"
+}
+
+//class MyTest {
+//
+//    private lateinit var myService: MyService
+//
+//    @Before
+//    fun setup() {
+//        myService = MyService()
+//    }
+//
+//    @Test
+//    fun testAction() {
+//        Assert.assertEqulas("JinWon", myService.performAction())
+//    }
+//
+//}
+
