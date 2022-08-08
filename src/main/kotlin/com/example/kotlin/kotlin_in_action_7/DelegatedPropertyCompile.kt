bce47c2f221e5ae1d53151cc7499f9b172c0a7b3
ljw0829 <olo_jinwon@naver.com>
2022-08-08 20:41:57 +0900

    - feat : 프로퍼티 값을 맵에 저장

--- src/main/kotlin/com/example/kotlin/kotlin_in_action_7/DelegatedPropertyCompile.kt
+++ src/main/kotlin/com/example/kotlin/kotlin_in_action_7/DelegatedPropertyCompile.kt
@@ -1,7 +1,10 @@
 package com.example.kotlin.kotlin_in_action_7
 
 fun main() {
-
+    val p =  PropertyPerson()
+    val data = mapOf("name" to "진원", "company" to "마이다스")
+    data.forEach { (attrName, value) -> p.setAttributes(attrName, value) }
+    println(p.name)
 }
 
 //class C {
@@ -12,3 +15,12 @@ fun main() {
 //}
 //
 //val c = C()
+
+class PropertyPerson {
+    private val _attributes = hashMapOf<String, String>()
+    fun setAttributes(attrName: String, value: String) {
+        _attributes[attrName] = value
+    }
+
+    val name: String by _attributes
+}
