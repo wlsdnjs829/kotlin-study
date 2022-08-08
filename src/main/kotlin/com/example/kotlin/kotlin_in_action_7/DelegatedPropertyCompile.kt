1f36c116cb7a993a7cea07a598a9862dd0b28e71
ljw0829 <olo_jinwon@naver.com>
2022-08-08 20:32:16 +0900

    - feat : 위임 프로퍼티 컴파일 규칙

---
+++ src/main/kotlin/com/example/kotlin/kotlin_in_action_7/DelegatedPropertyCompile.kt
@@ -0,0 +1,14 @@
+package com.example.kotlin.kotlin_in_action_7
+
+fun main() {
+
+}
+
+//class C {
+//    private val <delegate> = MyDelegate()
+//    var prop: Type
+//        get() = <delegate>.getValue(this, <property>)
+//        set(value : Type) = <delegate>.setValue(this, <property>, value )
+//}
+//
+//val c = C()
