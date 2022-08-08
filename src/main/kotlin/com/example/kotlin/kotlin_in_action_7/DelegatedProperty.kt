a40ae89914709128c62c353086b9e0940c11f29b
ljw0829 <olo_jinwon@naver.com>
2022-08-08 20:13:16 +0900

    - feat : 위임 프로퍼티 구현

--- src/main/kotlin/com/example/kotlin/kotlin_in_action_7/DelegatedProperty.kt
+++ src/main/kotlin/com/example/kotlin/kotlin_in_action_7/DelegatedProperty.kt
@@ -1,5 +1,8 @@
 package com.example.kotlin.kotlin_in_action_7
 
+import java.beans.PropertyChangeListener
+import java.beans.PropertyChangeSupport
+import kotlin.properties.Delegates
 import kotlin.reflect.KProperty
 
 fun main() {
@@ -8,17 +11,55 @@ fun main() {
     println(oldValue)
 
     foo.p = "hi"
+
+    val person = ChangePerson("진원", 28, 2000)
+    person.addPropertyChangeListener { evt ->
+        println("프로퍼티 ${evt.propertyName} 변경됨. ${evt.oldValue}에서 ${evt.newValue}으로")
+    }
+
+    person.age = 29
+    person.salary = 3000
 }
 
 class Foo {
     var p: Any by Delegate()
-}class Delegate {
+}
+
+class Delegate {
     operator fun getValue(foo: Foo, property: KProperty<*>): Any {
         return "jinwon"
     }
 
-    operator fun setValue(foo: Foo, property: KProperty<*>, any: Any) {
+    operator fun setValue(foo: Foo, property: KProperty<*>, any: Any) {}
+}
+
+class ObservableProperty(var propValue: Int, private val changeSupport: PropertyChangeSupport) {
+    operator fun getValue(p: ChangePerson, prop: KProperty<*>) = propValue
+    operator fun setValue(p: ChangePerson, prop: KProperty<*>, newValue: Int) {
+        val oldValue = propValue
+        propValue = newValue
+        changeSupport.firePropertyChange(prop.name, oldValue, newValue)
     }
 }
 
+open class PropertyChangeAware {
+    val changeSupport by lazy { PropertyChangeSupport(this) }
 
+    fun addPropertyChangeListener(listener: PropertyChangeListener) {
+        changeSupport.addPropertyChangeListener(listener)
+    }
+
+    fun removePropertyChangeListener(listener: PropertyChangeListener) {
+        changeSupport.removePropertyChangeListener(listener)
+    }
+}
+
+class ChangePerson(val name: String, age: Int, salary: Int) : PropertyChangeAware() {
+    private val observer = {
+        prop: KProperty<*>, oldValue: Int, newValue : Int ->
+        changeSupport.firePropertyChange(prop.name, oldValue, newValue)
+    }
+
+    var age: Int by Delegates.observable(age, observer)
+    var salary: Int by Delegates.observable(salary, observer)
+}
