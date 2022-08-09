3080d831225c6d00fbee57c3abf6f89598946daf
ljw0829 <olo_jinwon@naver.com>
2022-08-09 20:03:05 +0900

    - feat : 인자로 받은 함수 호출

---
+++ src/main/kotlin/com/example/kotlin/kotlin_in_action_8/FunctionType.kt
@@ -0,0 +1,24 @@
+package com.example.kotlin.kotlin_in_action_8
+
+fun main() {
+    println("ab1c".filter { it in 'a'..'z' })
+}
+
+fun twoAndThree(operation: (Int, Int) -> Int) {
+    val result = operation(2, 3)
+    println("결과는 $result")
+}
+
+fun String.filter(predicate: (Char) -> Boolean) : String {
+    val sb = StringBuilder()
+
+    for (index in 0 until length) {
+        val element = get(index)
+
+        if (predicate(element)) {
+            sb.append(element)
+        }
+    }
+
+    return sb.toString()
+}
