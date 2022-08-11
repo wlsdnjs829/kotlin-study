60a86241b393802517fdf15efb699da5419903bb
ljw0829 <ljw0829@midasin.com>
2022-08-11 18:06:13 +0900

    - feat : 자원 관리를 위해 인라인된 람다 사용

--- src/main/kotlin/com/example/kotlin/kotlin_in_action_8/Inlining.kt
+++ src/main/kotlin/com/example/kotlin/kotlin_in_action_8/Inlining.kt
@@ -1,6 +1,9 @@
 package com.example.kotlin.kotlin_in_action_8
 
+import java.io.BufferedReader
+import java.io.FileReader
 import java.util.concurrent.locks.Lock
+import kotlin.concurrent.withLock
 
 fun lock(l: Lock) {
     println("before")
@@ -26,4 +29,8 @@ class LockOwner(private val lock: Lock) {
     fun runUnderLock(body: () -> Unit) {
         synchronized(lock, body)
     }
+}
+
+fun readFirstLineFromFile(path:String): String {
+    BufferedReader(FileReader(path)).use { br -> return br.readLine() }
 }
