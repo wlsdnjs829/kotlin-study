8f2efa4a6077d0a7cbe854cee1718b6378dc2263
ljw0829 <olo_jinwon@naver.com>
2022-08-13 19:16:09 +0900

    - feat : 타입 파라미터를 널이 될 수 없는 타입으로 한정

---
+++ src/main/kotlin/com/example/kotlin/kotlin_in_action_9/GenericTypeNotNull.kt
@@ -0,0 +1,16 @@
+package com.example.kotlin.kotlin_in_action_9
+
+fun main() {
+    println(nullProcessor())
+}
+
+fun nullProcessor() {
+    val nullableStringProcessor = Processor<String>()
+    nullableStringProcessor.process("진원")
+}
+
+class Processor<T : Any> {`
+    fun process(value: T) {
+        value.hashCode()
+    }
+}
