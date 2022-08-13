427adb71733fc289c2b223eaa040412d1acea545
ljw0829 <olo_jinwon@naver.com>
2022-08-13 19:58:01 +0900

    - feat : 실체화한 타입 파라미터를 사용한 함수 선언

--- src/main/kotlin/com/example/kotlin/kotlin_in_action_9/GenericTypeCast.kt
+++ src/main/kotlin/com/example/kotlin/kotlin_in_action_9/GenericTypeCast.kt
@@ -6,6 +6,11 @@ fun main() {
     printSum(listOf(1, 2, 3))
 //    printSum(setOf(1, 2, 3))
 //    printSum(listOf("a", "b", "c"))
+
+    println(isA<String>(123))
+
+    val items = listOf("one", 2, "three")
+    println(items.filterIsInstance<String>())
 }
 
 fun printSum(c: Collection<Int>) {
@@ -13,3 +18,5 @@ fun printSum(c: Collection<Int>) {
         println(c.sum())
     }
 }
+
+inline fun <reified T> isA(value: Any) = value is T
