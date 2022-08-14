907fbaabd807e2175f516433deb77695e2dc297c
ljw0829 <olo_jinwon@naver.com>
2022-08-14 19:46:35 +0900

    - feat : 공변성: 하위 타입 관계를 유지

---
+++ src/main/kotlin/com/example/kotlin/kotlin_in_action_9/Covariant.kt
@@ -0,0 +1,40 @@
+package com.example.kotlin.kotlin_in_action_9
+
+fun main() {
+
+}
+
+interface Producer<out T> {
+    fun producer(): T
+}
+
+open class Animal {
+    fun feed() {}
+}
+
+class Herd<out T : Animal>(vararg animals: T) {
+    val size: Int get() = this.size
+    operator fun get(i: Int): T = this[i]
+}
+
+fun feedAll(animals: Herd<Animal>) {
+    for (i in 0 until animals.size) {
+        animals[i].feed()
+    }
+}
+
+class Cat : Animal() {
+    fun cleanLitter() {}
+}
+
+fun takeCareOfCats(cats: Herd<Cat>) {
+    for (i in 0 until cats.size) {
+        cats[i].cleanLitter()
+    }
+
+    feedAll(cats)
+}
+
+interface MutableList<T> : List<T>, MutableCollection<T> {
+    override fun add(element: T): Boolean
+}
