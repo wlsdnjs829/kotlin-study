fdf6679aa895c5ee692922c7a7568349552cef7d
ljw0829 <olo_jinwon@naver.com>
2022-08-17 17:24:25 +0900

    - feat : 애노테이션 대상

---
+++ src/main/kotlin/com/example/kotlin/kotiln_in_action_10/AnnotationTarget.kt
@@ -0,0 +1,11 @@
+package com.example.kotlin.kotiln_in_action_10
+
+//class HasTempFolder {
+//    @get:Rule
+//    val folder = TemporaryFolder()
+//
+//    @Test
+//    fun testUsingTempFolder() {
+//        val ~
+//    }
+//}
