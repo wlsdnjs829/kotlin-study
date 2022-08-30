501e67c0713d4dbef83ba8e8db3336f6c95f50cb
ljw0829 <olo_jinwon@naver.com>
2022-08-30 20:02:41 +0900

    - feat : 멤버 확장 함수: SQL을 위한 내부 DSL

---
+++ src/main/kotlin/com/example/kotlin/kotlin_in_action_11/DslDb.kt
@@ -0,0 +1,16 @@
+package com.example.kotlin.kotlin_in_action_11
+
+fun main() {
+}
+
+//object Country: Table() {
+//    val id = integer("id").autoIncrement().primaryKey()
+//    val name = varchar("name", 50)
+//}
+//
+//class Table {
+//    fun integer(name :String) : Column<Int>
+//    fun varchar(name :String, length: Int) : Column<String>
+//    fun <T> Column<T>.primaryKey(): Column<T>
+//    fun Column<Int>.autoIncrement(): Column<Int>
+//}
