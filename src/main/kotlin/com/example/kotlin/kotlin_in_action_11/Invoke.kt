7d4042f3cb7c440c9ef9b5df4609cb0f2770fd84
ljw0829 <olo_jinwon@naver.com>
2022-08-30 18:33:33 +0900

    - feat : invoke 관례와 함수형 타입

--- src/main/kotlin/com/example/kotlin/kotlin_in_action_11/Invoke.kt
+++ src/main/kotlin/com/example/kotlin/kotlin_in_action_11/Invoke.kt
@@ -3,10 +3,34 @@ package com.example.kotlin.kotlin_in_action_11
 fun main() {
     val greeter = Greeter("반갑습니다")
     greeter("진원")
+
+    val i1 = Issue("IDEA-12345", "IDEA", "Bug", "Major", "환경 설정 저장 실패")
+    val i2 = Issue("KT-12345", "Kotlin", "Feature", "Normal", "평범한 이슈")
+
+    val predicate = ImportantIssuesPredicate("IDEA")
+
+    for (issue in listOf(i1, i2).filter(predicate)) {
+        println(issue.id)
+    }
 }
 
 class Greeter(private val greeting: String) {
     operator fun invoke(name: String) {
         println("$greeting, $name")
+    }
+}
+
+data class Issue(
+    val id: String, val project: String, val type: String,
+    val priority: String, val description: String
+)
+
+class ImportantIssuesPredicate(private val project: String) : (Issue) -> Boolean {
+    override fun invoke(issue: Issue): Boolean {
+        return issue.project == project && issue.isImportant()
+    }
+
+    private fun Issue.isImportant(): Boolean {
+        return type == "Bug" && (priority == "Major" || priority == "Critical")
     }
 }
