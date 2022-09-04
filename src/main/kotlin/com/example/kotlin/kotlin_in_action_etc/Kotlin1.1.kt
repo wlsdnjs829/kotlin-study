66bc87a74e3d975bd1edc8b3bac568747046872e
ljw0829 <olo_jinwon@naver.com>
2022-09-04 20:58:12 +0900

    - feat : 로컬 변수 등을 위임

--- src/main/kotlin/com/example/kotlin/kotlin_in_action_etc/Kotlin1.1.kt
+++ src/main/kotlin/com/example/kotlin/kotlin_in_action_etc/Kotlin1.1.kt
@@ -1,8 +1,11 @@
 package com.example.kotlin.kotlin_in_action_etc
 
 import kotlin.math.sqrt
+import kotlin.reflect.KProperty
 
-/* 타입 별명 */
+/**
+ * 타입 별명
+ */
 // 컬렉션의 인스턴스에 대한 타입 별명
 typealias Args = Array<String>
 
@@ -25,7 +28,9 @@ class Foo {
 
 typealias FooBarBaz = Foo.Bar.Baz
 
-/* 프로퍼티 접근자 인라이닝 */
+/**
+ * 프로퍼티 접근자 인라이닝
+ */
 val toplevel: Double
     inline get() = Math.PI
 
@@ -43,7 +48,9 @@ inline var InlinePropExample.square: Int
         value = sqrt(v.toDouble()).toInt()
     }
 
-/* 제네릭 타입으로 이넘 값 접근 */
+/**
+ *  제네릭 타입으로 이넘 값 접근
+ */
 enum class DAYSOFWEEK { MON, TUE, WED, THR, FRI, SAT, SUN }
 
 inline fun <reified T : Enum<T>> mkString() : String =
@@ -57,3 +64,21 @@ inline fun <reified T : Enum<T>> mkString() : String =
 //fun main() {
 //    println(mkString<DAYSOFWEEK>())
 //}
+
+/**
+ * 로컬 변수 등을 위임
+ */
+class BarDelegate {
+    operator fun getValue(thisRef: Any?, property: KProperty<*>) : Int {
+        println("thisRef = $thisRef")
+        println("property.name = ${property.name}")
+        return 100
+    }
+}
+
+val y: Int by BarDelegate()
+
+fun main() {
+    val x: Int by BarDelegate()
+    println("x = $x, y = $y")
+}
